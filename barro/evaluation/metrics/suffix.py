0508f4196ae200a5d5b5d6bae08fef8be148012b
Pedro Gamallo Fernández <pedro.gamallo.fernandez@usc.es>
2023-10-10 10:29:21 +0200

    Convert __levenshtein_similarity() in __damerau_levenshtein_similarity()

--- barro/evaluation/metrics/suffix.py
+++ barro/evaluation/metrics/suffix.py
@@ -37,7 +37,7 @@ def get_damerau_levenshtein_score(predictions: list[np.array], ground_truths: li
 
     list_dl_scores = []
     for pred, gt in zip(predictions, ground_truths):
-        dl_distance, len_preds, len_gts = __levenshtein_similarity(pred, gt, eoc)
+        dl_distance, len_preds, len_gts = __damerau_levenshtein_similarity(pred, gt, eoc)
         dl_score = 1 - (dl_distance / max(len_preds, len_gts))
         list_dl_scores.append(dl_score)
 
@@ -46,8 +46,8 @@ def get_damerau_levenshtein_score(predictions: list[np.array], ground_truths: li
     return dl_score
 
 
-def __levenshtein_similarity(predictions: np.array, ground_truths: np.array,
-                             code_end: Union[str, int]) -> (float, int, int):
+def __damerau_levenshtein_similarity(predictions: np.array, ground_truths: np.array,
+                                     code_end: Union[str, int]) -> (float, int, int):
     if code_end:
         try:
             l1 = np.where(predictions == code_end)[0].item()
@@ -66,14 +66,20 @@ def __levenshtein_similarity(predictions: np.array, ground_truths: np.array,
 
     matrix = [list(range(l1 + 1))] * (l2 + 1)
 
-    for zz in list(range(l2 + 1)):
-        matrix[zz] = list(range(zz, zz + l1 + 1))
-    for zz in list(range(0,l2)):
-        for sz in list(range(0,l1)):
-            if predictions[sz] == ground_truths[zz]:
-                matrix[zz+1][sz+1] = min(matrix[zz+1][sz] + 1, matrix[zz][sz+1] + 1, matrix[zz][sz])
-            else:
-                matrix[zz+1][sz+1] = min(matrix[zz+1][sz] + 1, matrix[zz][sz+1] + 1, matrix[zz][sz] + 1)
+    for i in list(range(l2 + 1)):
+        matrix[i] = list(range(i, i + l1 + 1))
+
+    for i in range(1, l2 + 1):
+        for j in range(1, l1 + 1):
+            cost = 0 if predictions[j - 1] == ground_truths[i - 1] else 1
+            matrix[i][j] = min(matrix[i - 1][j] + 1,         # Deletion
+                               matrix[i][j - 1] + 1,         # Insertion
+                               matrix[i - 1][j - 1] + cost)  # Substitution
+
+            # Check for transposition
+            if i > 1 and j > 1 and predictions[j - 1] == ground_truths[i - 2] and \
+                    predictions[j - 2] == ground_truths[i - 1]:
+                matrix[i][j] = min(matrix[i][j], matrix[i - 2][j - 2] + cost)  # Transposition
 
     distance = float(matrix[l2][l1])
 
